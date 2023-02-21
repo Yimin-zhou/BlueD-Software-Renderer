4887bcd753d4950aecc7db08ccd580ad079f1128
Yimin ZHOU <55327029+Yimin-zhou@users.noreply.github.com>
2023-02-21 02:02:09 +0100

    add comptr

--- BlueD-Render/renderer/src/gui/gui.h
+++ BlueD-Render/renderer/src/gui/gui.h
@@ -4,6 +4,7 @@
 #include <d3d12.h>
 #include <dxgi1_4.h>
 #include <cstdint>
+#include <wrl/client.h>
 
 #include "imgui.h"
 #include "imgui_impl_win32.h"
@@ -28,15 +29,15 @@ namespace blueGUI
 	extern int32_t const                NUM_FRAMES_IN_FLIGHT;
 	extern uint32_t                     g_frameIndex;
 	extern int32_t const                NUM_BACK_BUFFERS ;
-	extern ID3D12Device*				g_pd3dDevice;
-	extern ID3D12DescriptorHeap*		g_pd3dRtvDescHeap;
-	extern ID3D12DescriptorHeap*		g_pd3dSrvDescHeap;
-	extern ID3D12CommandQueue*			g_pd3dCommandQueue;
-	extern ID3D12GraphicsCommandList*	g_pd3dCommandList;
-	extern ID3D12Fence*					g_fence;
+	extern Microsoft::WRL::ComPtr<ID3D12Device>				g_pd3dDevice;
+	extern Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>		g_pd3dRtvDescHeap;
+	extern Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>		g_pd3dSrvDescHeap;
+	extern Microsoft::WRL::ComPtr<ID3D12CommandQueue>			g_pd3dCommandQueue;
+	extern Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList>	g_pd3dCommandList;
+	extern Microsoft::WRL::ComPtr<IDXGISwapChain3>				g_pSwapChain;
+	extern Microsoft::WRL::ComPtr<ID3D12Fence>					g_fence;
 	extern HANDLE                       g_fenceEvent;
 	extern uint64_t                     g_fenceLastSignaledValue;
-	extern IDXGISwapChain3*				g_pSwapChain;
 	extern HANDLE                       g_hSwapChainWaitableObject;
 
 	void CreateHWindow();
